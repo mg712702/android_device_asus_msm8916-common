90620ac1d5bc6e693cfdb9d7721af22032968183
Diego Wilson <diegowilson@google.com>
2017-02-24 17:07:28 -0800

    msm8916: gps: Move device dependent modules to /vendor
    Change-Id: I8229a6e4a9512a03cbc6c9522bb1dc0ce988d1c9
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- gps/loc_api/libloc_api_50001/Android.mk
+++ gps/loc_api/libloc_api_50001/Android.mk
@@ -7,6 +7,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libloc_eng
 LOCAL_MODULE_OWNER := qcom
+LOCAL_VENDOR_MODULE := true
 
 LOCAL_MODULE_TAGS := optional
 
@@ -68,6 +69,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := gps.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_OWNER := qcom
+LOCAL_VENDOR_MODULE := true
 
 LOCAL_MODULE_TAGS := optional
 
