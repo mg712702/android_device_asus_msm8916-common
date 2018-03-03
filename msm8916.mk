d3b88dffec7aa0e4b8eae3a954c9461f2bef43b1
Kevin Rocard <krocard@google.com>
2018-03-02 19:41:31 -0800

    msm8916-common: Migrate to audio V4 implementation
    * This puts an end to the goddamn logspam begging for 4.0 impl
    
    Test: regression test on the board
    Bug: 38184704
    Change-Id: Ife559125850277865451f92ea862e7ec47fc9da7
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -62,9 +62,9 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     android.hardware.soundtrigger@2.0-service
 
