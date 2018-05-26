4a7c998a0106d5d4136498e41fcb30e4132a32cd
Michael Bestas <mkbestas@lineageos.org>
2018-05-26 21:01:02 +0300

    msm8916-common: Build libmm-omxcore
    * libOmxVpp needs it
    
    Change-Id: I5e557aa6ad1b16a9a24b2d554af921da079fb250
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -214,6 +214,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 PRODUCT_PACKAGES += \
+    libmm-omxcore \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
