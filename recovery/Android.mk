42de329d11ba5f2f9bda6a058d0d75538bb1f749
Nicholas Lim <nicknitewolf@lineageos.org>
2018-08-09 04:27:12 +0800

    msm8916-common: recovery: Fix include headers
    Change-Id: I3800e64b727cae0013a1a95c6d16d56ab34ece29
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -2,7 +2,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := $(call project-path-for,recovery) \
-    $(call project-path-for,recovery)/updater/include
+    bootable/recovery/updater/include \
+    bootable/recovery/edify/include \
+    bootable/recovery/otautil/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_asus
 LOCAL_MODULE_TAGS := eng
