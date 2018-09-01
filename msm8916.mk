6d8bb6edc511d6586ad5ccfdfcc90ce68ab6e920
Daniel Jarai <jaraidaniel@gmail.com>
2018-09-02 00:34:14 +0200

    msm89116-common: Make the external camera provider ignore internal cameras
    * The external camera provider occupies our camera v4l2 nodes,
      potentially crashing the kernel driver and blocking the camera HAL.
      Unfortunately, there's no easy way to just disable it, so let's just
      tell it to ignore the internal video devices.
    
    Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81
    Signed-off-by: Daniel Jarai <jaraidaniel@gmail.com>
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -112,6 +112,9 @@ PRODUCT_PACKAGES += \
     camera.msm8916 \
     Snap
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 # Connectivity Engine support
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
