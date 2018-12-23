b32e96d0250339c999bf6ecc588bfaad7ece18b6
Harshit Jain <harshitjain6751@gmail.com>
2018-12-23 22:46:49 +0530

    msm8916-common: interactive: disable stream lock for now
    * Killed my cat while it was browsing fb and it showed down lot of videos in its newsfeed
    
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- rootdir/etc/init.qcom.power.sh
+++ rootdir/etc/init.qcom.power.sh
@@ -60,6 +60,7 @@ case "$soc_id" in
         echo 0 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
         echo "1 800000:85 998400:90 1094400:80" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
         echo 50000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
+        echo 0 > /sys/devices/system/cpu/cpufreq/interactive/InStreamFreq
 
         # Bring up all cores online
         echo 1 > /sys/devices/system/cpu/cpu1/online
