2ae1e55d03ac94fbf58de2eaf16db122002b3f71
Mrinal Ghosh <mg712702@gmail.com>
2019-12-01 10:22:01 +0000

    msm8916-common: Build configstore service
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -123,6 +123,7 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-service \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
